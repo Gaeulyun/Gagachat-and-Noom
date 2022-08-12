04c3bee3d8e2a9a89c04c1ec808325b918d0addb
Gaeulyun <loveyge@naver.com>
2022-08-12 23:37:37 +0900

    error

--- src/server.js
+++ src/server.js
@@ -17,22 +17,23 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-function handleConnection(socket) {
-    console.log(socket);
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
 }
+
 
 // 다른 브라우저 이용할 수 있도록 함
 const sockets = [];
 
 wss.on("connection", (socket) => {
-    socket.push(socket);
-    console.log("Connected to Browser ✔");
-    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-    socket.on('message', (message) => {
-        sockets.forEach(aSocket =>aSocket.send(message));
-        const messageString = message.toString('utf8');
-        socket.send(messageString);
-        });        
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (message) => {
+      const messageString = message.toString('utf8');
+      socket.send(messageString);
+    sockets.forEach((aSocket) => aSocket.send(message));
+  });
 });
 
 server.listen(3000, handleListen);
