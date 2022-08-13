d9baae53226c7a16bb9c5a86ff4f07886194ebaa
Gaeulyun <loveyge@naver.com>
2022-08-13 22:21:47 +0900

    #2.9 Room Count part Two

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
+
 const app = express();
+
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
+
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
@@ -34,11 +37,15 @@ wsServer.on("connection", (socket) => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) =>
       socket.to(room).emit("bye", socket.nickname)
     );
+  });
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("new_message", (msg, room, done) => {
     socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
